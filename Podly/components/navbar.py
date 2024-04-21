93f354960237f3966d776945be14bfdec212db51
Bill Susanto <billsusanto@Bills-Air.lan>
2024-04-21 05:43:04 -0700

    Finished about and navbar

--- Podly/components/navbar.py
+++ Podly/components/navbar.py
@@ -5,23 +5,56 @@ from Podly.components.callout import callout
 
 import reflex as rx
 
+def about_button() -> rx.Component:
+    return rx.link(
+        rx.button(
+            rx.hstack(
+                rx.text("About", font_size="16px", color="rgb(173,173,173)",),
+                align="center",
+                color="white",
+            ),
+            padding_y="1.5em",
+            border_radius="1em",
+            background_color="rgb(32,29,29)",
+            style={"border-style": "solid", "border-width": "thin","border-color": "rgb(173,173,173)"},
+        ),
+        href="/about"
+    )
 
 def discord_button() -> rx.Component:
-    return rx.button(
-        
+    return rx.link(
+        rx.button(
+            rx.hstack(
+                rx.image(src="/icons/discord.svg", width="24px"),
+                align="center",
+            ),
+            padding_y="1.5em",
+            border_radius="1em",
+            height="42px",
+            background_color="rgb(32,29,29)",
+            style={"border-style": "solid", "border-width": "thin","border-color": "rgb(173,173,173)"},
+        ),
+        href="https://github.com/WilsonLimSet/PodcastTest",
+        target="_blank",
     )
 
 def github_button() -> rx.Component:
-    return rx.button(
-        rx.hstack(
-            rx.image(src="icons/github.svg", width="24px"),
-            rx.text("GitHub", font_size="16px"),
-            callout("15k"),
-            align="center",
+    return rx.link(
+        rx.button(
+            rx.hstack(
+                rx.image(src="/icons/github.svg", width="24px"),
+                rx.text("GitHub", font_size="16px", color="rgb(173,173,173)",),
+                callout("15k"),
+                align="center",
+            ),
+            padding_y="1.5em",
+            padding_x="2em",
+            border_radius="1em",
+            background_color="rgb(32,29,29)",
+            style={"border-style": "solid", "border-width": "thin","border-color": "rgb(173,173,173)"},
         ),
-        padding_y="1.5em",
-        padding_x="2em",
-        border_radius="1em",
+        href="https://github.com/WilsonLimSet/PodcastTest",
+        target="_blank",
     )
 
 def navbar() -> rx.Component:
@@ -29,14 +62,17 @@ def navbar() -> rx.Component:
         rx.hstack(
             rx.hstack(
                 rx.logo(),
-                rx.heading("About"),
+                about_button(),
+                align="center",
             ),
             rx.spacer(),
             rx.hstack(
                 github_button(),
-                rx.heading("Discord"),
+                discord_button(),
             ),
             width="100vw",
             align="center",
         ),
+        background_color="rgb(32,29,29)",
+        style={"position": "fixed", "top": "0", "width": "100%"},
     )
